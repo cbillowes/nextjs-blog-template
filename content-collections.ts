69bbad1a734a61605b286be8f222636738ec51bb
Clarice Bouwer <cbillowes@gmail.com>
2025-11-30 12:59:00 +0400

    Configure prev and next posts

--- content-collections.ts
+++ content-collections.ts
@@ -13,6 +13,17 @@ const posts = defineCollection({
     summary: z.string(),
     content: z.string(),
   }),
+  transform: async (doc, { collection }) => {
+    const docs = (await collection.documents()).sort((a, b) =>
+      a._meta.fileName.localeCompare(b._meta.fileName),
+    );
+    const idx = docs.findIndex((d) => doc._meta.filePath === d._meta.filePath);
+    return {
+      ...doc,
+      prev: idx > 0 ? docs[idx - 1] : null,
+      next: idx < docs.length - 1 ? docs[idx + 1] : null,
+    };
+  },
 });
 
 export default defineConfig({
