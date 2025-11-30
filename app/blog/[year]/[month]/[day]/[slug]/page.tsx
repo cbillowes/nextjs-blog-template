2c1b2f3c57ce069e4ee1b9a6b3670cf7dd9b6622
Clarice Bouwer <cbillowes@gmail.com>
2025-11-30 18:47:51 +0400

    Create blog post content page

---
+++ app/blog/[year]/[month]/[day]/[slug]/page.tsx
@@ -0,0 +1,39 @@
+import { allPosts, Post } from 'content-collections';
+import Image from 'next/image';
+
+export default async function BlogPostPage({
+  params,
+}: {
+  params: { year: string; month: string; day: string; slug: string };
+}) {
+  const { year, month, day, slug } = await params;
+
+  const isMatch = (post: Post) =>
+    post.slug.endsWith(`${year}/${month}/${day}/${slug}`);
+
+  const post = allPosts.find(isMatch);
+  if (!post) {
+    return <div>The post cannot be found.</div>;
+  }
+  return (
+    <div>
+      <Image
+        src={post.hero}
+        width={1024}
+        height={800}
+        alt="Hero image"
+        className="object-cover w-full h-128 mb-4"
+      />
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-center text-7xl font-bold">{post.title}</h1>
+        <p className="text-center text-gray-500 mb-8">
+          {post.date} &middot; {post.timeToRead}
+        </p>
+        <div className="prose prose-lg mx-auto">
+          <p>{post.summary}</p>
+          <div id="post" dangerouslySetInnerHTML={{ __html: post.content }} />
+        </div>
+      </div>
+    </div>
+  );
+}
