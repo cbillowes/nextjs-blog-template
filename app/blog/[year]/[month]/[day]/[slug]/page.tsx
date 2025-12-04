23c9f5640145ec7ad05c6c0c13b1b9927d226d0b
Clarice Bouwer <cbillowes@gmail.com>
2025-12-04 22:00:51 +0400

    Add page metadata to blog page

--- app/blog/[year]/[month]/[day]/[slug]/page.tsx
+++ app/blog/[year]/[month]/[day]/[slug]/page.tsx
@@ -2,17 +2,70 @@ import { Markdown } from '@/components/markdown';
 import { allPosts, Post } from 'content-collections';
 import Image from 'next/image';
 
-export default async function BlogPostPage({
-  params,
-}: {
-  params: { year: string; month: string; day: string; slug: string };
-}) {
-  const { year, month, day, slug } = await params;
+const SITE_URL = process.env.NEXT_PUBLIC_WEBSITE_URL;
 
-  const isMatch = (post: Post) =>
-    post.slug.endsWith(`${year}/${month}/${day}/${slug}`);
+type Props = {
+  params: {
+    year: string;
+    month: string;
+    day: string;
+    slug: string;
+  };
+};
 
-  const post = allPosts.find(isMatch);
+async function isPostMatch(props: Props) {
+  const { year, month, day, slug } = await props.params;
+  return (post: Post) => post.slug.endsWith(`/${year}/${month}/${day}/${slug}`);
+}
+
+export async function generateMetadata({ params }: Props) {
+  const post = allPosts.find(await isPostMatch({ params }));
+  if (!post) return;
+
+  const canonicalUrl = `${SITE_URL}/${post.slug}`;
+  const imageUrl = `${SITE_URL}/${post.hero}`;
+  return {
+    title: post.title,
+    authors: [{ name: 'Clarice Bouwer' }],
+    description: `${post.summary.substring(0, 140)}${
+      post.summary.length > 140 ? '...' : ''
+    }`,
+    imageUrl,
+    url: canonicalUrl,
+    type: 'article',
+    openGraph: {
+      siteName: 'My Next.js Blog',
+      title: post.title,
+      description: post.summary,
+      url: canonicalUrl,
+      images: [
+        {
+          url: imageUrl,
+          width: 1200,
+          height: 630,
+          alt: post.title,
+          // You could create a preprocessor to convert all your images to png for your og:image
+          // https://github.com/cbillowes/curious-programmer-mercury/blob/main/scripts/process-webp.js
+          type: 'image/png',
+        },
+      ],
+      type: 'article',
+      publishedTime: post.date,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: post.title,
+      description: post.summary,
+      images: [imageUrl],
+    },
+    alternates: {
+      canonical: canonicalUrl,
+    },
+  };
+}
+
+export default async function BlogPostPage({ params }: Props) {
+  const post = allPosts.find(await isPostMatch({ params }));
   if (!post) {
     return <div>The post cannot be found.</div>;
   }
