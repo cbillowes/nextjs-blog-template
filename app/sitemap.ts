5993caac2a077fa75444682f7d0153287701b303
Clarice Bouwer <cbillowes@gmail.com>
2025-12-04 06:04:15 +0400

    Simplify sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,31 +5,25 @@ import path from 'path';
 
 const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL!;
 
+// Ensures the URL is always an absolute URL
 function getImageUrl(image: string | undefined) {
   if (!image) return undefined;
+  // Determines if the image is an absolute URL or a relative path
   const url = image.startsWith('http') ? image : `${SITE_URL}${image}`;
+  // Fix breaking XML characters
   return url.replace(/&/g, '&amp;');
 }
 
-function getSitemapEntry(
-  slug: string,
-  date: string,
-  image: string | undefined,
-) {
-  return {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts: MetadataRoute.Sitemap = allPosts.map(({ slug, date, hero }) => ({
     url: `${SITE_URL}${slug}`,
     lastModified: new Date(date),
-    changeFrequency: 'yearly' as const,
-    priority: 0.7,
-    images: [getImageUrl(image)].filter(Boolean) as string[],
-  };
-}
+    changeFrequency: 'yearly' as const, // You can adjust as needed
+    priority: 0.7, // You can adjust as needed
+    images: [getImageUrl(hero)].filter(Boolean) as string[],
+  }));
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const posts: MetadataRoute.Sitemap = allPosts.map((a) =>
-    getSitemapEntry(a.slug, a.date, a.hero),
-  );
-
+  // Used to get the last modified date of the home page
   const homePageStat = fs.statSync(path.join(process.cwd(), 'app', 'page.tsx'));
   return [
     {
